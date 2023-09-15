222029cdaeb930f1fef7de5e528924839c458212
aiien61 <aiien61will@gmail.com>
2023-09-16 00:51:36 +0800

    move movies, directors, actors, move_revenues, movies_actors tables into movie_data script

--- create_tables.sql
+++ create_tables.sql
@@ -1,53 +1,8 @@
--- CREATE TABLE directors
+-- CREATE TABLE practice
 
-CREATE TABLE directors (
+CREATE TABLE practice (
 	
-	director_id SERIAL PRIMARY KEY,
-	first_name VARCHAR(30),
-	last_name VARCHAR(30) NOT NULL,
-	date_of_birth DATE,
-	nationality VARCHAR(20)
-);
-
--- CREATE TABLE actors
-
-CREATE TABLE actors (
-	
-	actor_id SERIAL PRIMARY KEY,
-	first_name VARCHAR(30),
-	last_name VARCHAR(30),
-	gender CHAR(1),
-	date_of_birth DATE
-);
-
--- CREATE TABLE movies
-
-CREATE TABLE movies (
-	
-	movie_id SERIAL PRIMARY KEY,
-	movie_name VARCHAR(50) NOT NULL,
-	movie_length INT,
-	movie_lang VARCHAR(20),
-	release_date DATE,
-	age_certificate VARCHAR(5),
-	director_id INT REFERENCES directors (director_id)
-);
-
--- CREATE TABLE movie_revenues
-
-CREATE TABLE movie_revenues (
-	
-	revenue_id SERIAL PRIMARY KEY,
-	movie_id INT REFERENCES movies (movie_id),
-	domestic_takings NUMERIC(6,2),
-	international_takings NUMERIC(6,2)
-);
-
--- CREATE TABLE movie_actors
-
-CREATE TABLE movies_actors (
-	
-	movie_id INT REFERENCES movies (movie_id),
-	actor_id INT REFERENCES actors (actor_id),
-	PRIMARY KEY (movie_id, actor_id)
+	id SERIAL PRIMARY KEY,
+	product_name VARCHAR(50),
+	product_price NUMERIC(4,2)
 );
