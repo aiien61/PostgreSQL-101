a12973444267bf3e0b951e48c0c5dd7c3f4cf771
aiien61 <aiien61will@gmail.com>
2023-09-22 15:29:46 +0800

    examples of grouping and having clauses

--- aggregation.sql
+++ aggregation.sql
@@ -81,3 +81,64 @@ WHERE nationality = 'Japanese';
 -- What is the average movie length for Chinese movies?
 SELECT AVG(movie_length) FROM movies
 WHERE movie_lang = 'Chinese';
+
+-- Grouping data
+
+/*
+SELECT column1, AGGFUN(column2) FROM tablename
+GROUP BY column1;
+*/
+
+SELECT COUNT(movie_lang) FROM movies;
+
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+GROUP BY movie_lang;
+
+SELECT movie_lang, AVG(movie_length) FROM movies
+GROUP BY movie_lang;
+
+SELECT movie_lang, age_certificate, AVG(movie_length) FROM movies
+GROUP BY movie_lang, age_certificate;
+
+SELECT movie_lang, age_certificate, AVG(movie_length) FROM movies
+WHERE movie_length > 120
+GROUP BY movie_lang, age_certificate;
+
+SELECT movie_lang, MIN(movie_length), MAX(movie_length) FROM movies
+WHERE age_certificate = '15'
+GROUP BY movie_lang;
+
+-- Having clauses
+
+/*
+SELECT column1, AGGFUN(column2) FROM tablename
+GROUP BY column1
+HAVING AGGFUN(column3) = value;
+*/
+
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+GROUP BY movie_lang
+HAVING COUNT(movie_lang) > 1;
+
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+WHERE movie_length > 120
+GROUP BY movie_lang
+HAVING COUNT(movie_lang) > 1;
+
+-- How many directors are there per nationality?
+SELECT nationality, COUNT(*) FROM directors
+GROUP BY nationality;
+
+-- What is the sum total movie length for each age certificate and movie language combination?
+SELECT age_certificate, movie_lang, SUM(movie_length) AS total_length FROM movies
+GROUP BY age_certificate, movie_lang;
+
+SELECT movie_lang, age_certificate, SUM(movie_length) AS total_length FROM movies
+GROUP BY movie_lang, age_certificate
+ORDER BY movie_lang, age_certificate;
+
+
+-- Return the movie language which have a sum total movie language of over 500 minutes.
+SELECT movie_lang, SUM(movie_length) FROM movies
+GROUP BY movie_lang
+HAVING SUM(movie_length) > 500;
