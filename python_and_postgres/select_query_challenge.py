1a935f403fa22586f2cd9e40f78409181a94e891
aiien61 <aiien61will@gmail.com>
2024-04-11 23:55:19 +0800

    update

--- python_and_postgres/select_query_challenge.py
+++ python_and_postgres/select_query_challenge.py
@@ -188,6 +188,22 @@ WHERE date_of_birth BETWEEN '1960-01-01' AND '1969-12-31'
 ORDER BY last_name
 """
 
+# Intersect the first name, last name and date of birth columns in the directors and actors tables.
+QUERY = """
+SELECT first_name, last_name, date_of_birth FROM directors
+INTERSECT
+SELECT first_name, last_name, date_of_birth FROM actors;
+"""
+
+# Retrieve the first names of male actors unless they have the same first name as any British directors.
+QUERY = """
+SELECT first_name FROM actors
+WHERE gender = 'M'
+EXCEPT
+SELECT first_name FROM directors
+WHERE nationality = 'British';
+"""
+
 def select(cursor) -> None:
     cursor.execute(QUERY)
     return None
