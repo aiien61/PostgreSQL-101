080e7ce76f3a4c04a322b21d93aabc1a84731e06
aiien61 <aiien61will@gmail.com>
2024-04-11 17:43:08 +0800

    full join

---
+++ python_and_postgres/full_join.py
@@ -0,0 +1,57 @@
+import json
+import pandas as pd
+import psycopg2 as pg
+
+QUERY = """
+SELECT d.director_id, d.first_name, d.last_name, mo.movie_name 
+FROM movies mo
+FULL JOIN directors d ON d.director_id = mo.director_id;
+"""
+
+QUERY = """
+SELECT d.director_id, d.first_name, d.last_name, mo.movie_name 
+FROM directors d
+FULL JOIN movies mo ON d.director_id = mo.director_id;
+"""
+
+QUERY = """
+SELECT d.director_id, d.first_name, d.last_name, mo.movie_name 
+FROM directors d
+FULL JOIN movies mo ON d.director_id = mo.director_id
+WHERE mo.movie_lang IN ('German', 'Korean')
+ORDER BY d.last_name;
+"""
+
+
+def select(cursor) -> None:
+    cursor.execute(QUERY)
+    return None
+
+
+def main():
+    try:
+        with open("config.json", "r") as f:
+            config = json.load(f)
+        
+        connection = pg.connect(**config, dbname="movie_data")
+        cursor = connection.cursor()
+        
+        select(cursor)
+        
+        connection.commit()
+
+        column_names = [col.name for col in cursor.description]
+        result = pd.DataFrame(cursor.fetchall(), columns=column_names)
+        print(result)
+        
+    except (Exception, pg.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+    
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+if __name__ == '__main__':
+    main()
