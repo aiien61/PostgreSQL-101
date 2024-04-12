33485f4fe910c4690c8e996ea523fe44671f6f49
aiien61 <aiien61will@gmail.com>
2024-04-12 15:48:51 +0800

    reverse string function

---
+++ python_and_postgres/reverse_string_function.py
@@ -0,0 +1,45 @@
+import json
+import pandas as pd
+import psycopg2 as pg
+
+QUERY = """
+SELECT REVERSE('reverse');
+"""
+
+QUERY = """
+SELECT movie_name, REVERSE(movie_name) FROM movies;
+"""
+
+
+def select(cursor) -> None:
+    cursor.execute(QUERY)
+    return None
+
+
+def main():
+    try:
+        with open("config.json", "r") as f:
+            config = json.load(f)
+        
+        connection = pg.connect(**config, dbname="movie_data_clone")
+        cursor = connection.cursor()
+        
+        select(cursor)
+        
+        connection.commit()
+
+        column_names = [col.name for col in cursor.description]
+        result = pd.DataFrame(cursor.fetchall(), columns=column_names)
+        print(result)
+        
+    except (Exception, pg.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+    
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+if __name__ == '__main__':
+    main()
